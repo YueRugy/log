b17ff380faa10d0a03096ac9904a3fb4d79dc70c
yue <yueweichao0927@gmail.com>
2020-03-28 19:39:05 +0800

    tailf 简单使用

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/Shopify/sarama v1.26.1 // indirect
-	github.com/hpcloud/tail v1.0.0 // indirect
+	github.com/hpcloud/tail v1.0.0
 	golang.org/x/sys v0.0.0-20200327173247-9dae0f8f5775 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"github.com/hpcloud/tail"
+)
+
 func main() {
-	
+	fileNmae := "./myLog"
+	config := tail.Config{
+		Location:    &tail.SeekInfo{Offset: 0, Whence: 2},
+		ReOpen:      true,
+		MustExist:   false,
+		Poll:        true,
+		Pipe:        false,
+		RateLimiter: nil,
+		Follow:      true,
+		MaxLineSize: 0,
+		Logger:      nil,
+	}
+
+	tails, err := tail.TailFile(fileNmae, config)
+	if err != nil {
+		fmt.Println("tail file failed ", err)
+	}
+
+	var (
+		line *tail.Line
+		ok   bool
+	)
+	for {
+		line, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen ,fileNmae =%s", fileNmae)
+		}
+		fmt.Println(line.Text)
+	}
 }
